9e0c15b21d21ae8b6c0a524788259215801c064a
Jennings Zhang <jenni_zh@protonmail.com>
2023-02-26 13:14:43 -0500

    Resolve ordering of siblings

--- validate_pipeline_by_scheduling_it.py
+++ validate_pipeline_by_scheduling_it.py
@@ -24,6 +24,17 @@ for i, p in enumerate(unverified_json_data['plugin_tree']):
     if 'plugin_parameter_defaults' not in p:
         p['plugin_parameter_defaults'] = []
 
+    # collect all previous, which includes the "previous_index"
+    # and also the "--plugininstances" parameter value
+    p['all_previous'] = set()
+
+    # every non-root member has at least one previous index
+    p['all_previous'].add(p['previous_index'])
+    # ts-plugins might have more than one.
+    for param in p['plugin_parameter_defaults']:
+        if param['name'] == 'plugininstances':
+            p['all_previous'].update(map(int, param['default'].split(',')))
+
 
 # We need to sort these plugins into breadth-first order.
 # I'm not going to use an efficient algorithm here, I'm just going to do one
@@ -48,39 +59,40 @@ queue = [0]
 while len(bfs_order) < len(unverified_json_data['plugin_tree']):
     if not queue:
         print('pipeline is invalid: disconnected!')
+        print([p['list_index'] for p in bfs_order])
         sys.exit(1)
     previous_index = queue.pop(0)
-    for p in unverified_json_data['plugin_tree']:
-        if p['previous_index'] == previous_index:
-            bfs_order.append(p)
-            queue.append(p['list_index'])
 
+    children = [p for p in unverified_json_data['plugin_tree'] if p['previous_index'] == previous_index]
+    # now we need to add all the children to bfs_order.
+    # however there might be dependency relationships between
+    # the children themselves, so we need to watch out for that.
+    children_who_are_dependent_on_siblings = []
 
-# Now we will "pretend to schedule" the pipeline in BFS order.
+    while children:
+        num_children = len(children)
+        for child in children:
+            visited_indexes = set(p['list_index'] for p in bfs_order)
+            if child['all_previous'] <= visited_indexes:
+                # Here is where child is being "visited" by BFS.
+                # We are adding child to the list `bfs_order` in BFS-order.
+                # If we wanted to, we could also create a plugin instance of "child" instead.
+                # In effect, that would be the equivalent of saving the list `bfs_order` to
+                # CUBE's database instead of a local variable in Python.
+                bfs_order.append(child)
+                queue.append(child['list_index'])
+            else:
+                children_who_are_dependent_on_siblings.append(child)
 
-root = bfs_order.pop(0)
-visited_indexes = set()
-visited_indexes.add(root['list_index'])
+        # keep retrying until all children have been added.
+        # if no children can be added, we're stuck either because of a cycle or a
+        # parent which doesn't belong to this tree.
+        if num_children == len(children_who_are_dependent_on_siblings):
+            print('pipeline is invalid: a previous is unvisited, possibly cyclic or has '
+                  'a parent which is not part of this pipeline!')
+            sys.exit(1)
+        children = children_who_are_dependent_on_siblings
+        children_who_are_dependent_on_siblings = []
 
-for p in bfs_order:
-    previous_indexes = set()
-
-    # every non-root member has at least one previous index
-    previous_indexes.add(p['previous_index'])
-    # ts-plugins might have more than one.
-    for param in p['plugin_parameter_defaults']:
-        if param['name'] == 'plugininstances':
-            previous_indexes.update(map(int, param['default'].split(',')))
-
-    if not previous_indexes <= visited_indexes:  # check if is subset
-        print('pipeline is invalid: a previous is unvisited, possibly cyclic or has '
-              'a parent which is not part of this pipeline!')
-        sys.exit(1)
-
-    # having visited an item in BFS order, then adding it to the list "visited"
-    # is comparable to having scheduled the item. Instead of maintaining the list
-    # "visited" ourselves, we would be creating a plugin instance in CUBE's database,
-    # and CUBE would do the above if-statement check for us!
-    visited_indexes.add(p['list_index'])
 
 print('pipeline is valid! it is a single, connected, directed acyclic graph.')
